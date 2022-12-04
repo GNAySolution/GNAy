9ca538254630727fe444aacdef07e562f0c1301c
GNAySolution <101186896+GNAySolution@users.noreply.github.com>
2022-12-04 10:43:06 +0800

    更新DataSalt
    1.Update:更新DataSalt

--- GNAy.Tools.WPF/GNAy.Tools.NET47/src/DataSalt.cs
+++ GNAy.Tools.WPF/GNAy.Tools.NET47/src/DataSalt.cs
@@ -10,6 +10,7 @@ namespace GNAy.Tools.NET47
     {
         public static byte Reverse(byte value)
         {
+            //https://ref.gamer.com.tw/redir.php?url=https%3A%2F%2Fiter01.com%2F536953.html
             value = (byte)(((value & 0xaa) >> 1) | ((value & 0x55) << 1));
             value = (byte)(((value & 0xcc) >> 2) | ((value & 0x33) << 2));
 
@@ -18,12 +19,33 @@ namespace GNAy.Tools.NET47
 
         public static uint Reverse(uint value)
         {
+            //https://ref.gamer.com.tw/redir.php?url=https%3A%2F%2Fiter01.com%2F536953.html
             value = ((value & 0xaaaaaaaa) >> 1) | ((value & 0x55555555) << 1);
             value = ((value & 0xcccccccc) >> 2) | ((value & 0x33333333) << 2);
             value = ((value & 0xf0f0f0f0) >> 4) | ((value & 0x0f0f0f0f) << 4);
             value = ((value & 0xff00ff00) >> 8) | ((value & 0x00ff00ff) << 8);
 
             return (value >> 16) | (value << 16);
+        }
+
+        public static byte ExclusiveOR(byte value, params byte[] keys)
+        {
+            foreach (byte key in keys)
+            {
+                value ^= key;
+            }
+
+            return value;
+        }
+
+        public static int ExclusiveOR(int value, params int[] keys)
+        {
+            foreach (int key in keys)
+            {
+                value ^= key;
+            }
+
+            return value;
         }
     }
 }
