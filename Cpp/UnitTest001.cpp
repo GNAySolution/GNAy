38f0ec4e9e6167e8734553c63d5c3242c9c578a5
GNAySolution <101186896+GNAySolution@users.noreply.github.com>
2022-12-11 08:19:54 +0800

    Update UnitTest001.cpp

--- Cpp/UnitTest001.cpp
+++ Cpp/UnitTest001.cpp
@@ -2,20 +2,22 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
+int main(const int argc, const char *argv[]) {
 
     char inputBuf1[8];
     memset(inputBuf1, 0, sizeof(inputBuf1));
 
-    const char iB1Format[] = {'%', sizeof(inputBuf1) - 1 + '0', 's', 0};
+    const char ib1fmt[] = {'%', sizeof(inputBuf1) - 1 + '0', 's', 0};
 
     printf("Hello World!\r\n");
 
-    scanf(iB1Format, inputBuf1);
+    scanf(ib1fmt, inputBuf1);
     printf("%s\r\n", inputBuf1);
 
+    printf("\r\nPress any key to exit.\r\n");
     memset(inputBuf1, 0, sizeof(inputBuf1));
-    scanf(iB1Format, inputBuf1);
+    scanf(ib1fmt, inputBuf1);
+    printf("%s\r\n", inputBuf1);
 
     usleep(3 * 1000 * 1000);
     return 0;
