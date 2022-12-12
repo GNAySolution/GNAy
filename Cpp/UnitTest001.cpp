de917a50c923e02494bd8584673de0dd71473eca
Yang <101186896+GNAySolution@users.noreply.github.com>
2022-12-12 22:35:33 +0800

    Update UnitTest001.cpp

--- Cpp/UnitTest001.cpp
+++ Cpp/UnitTest001.cpp
@@ -7,17 +7,25 @@ int main(const int argc, const char *argv[]) {
     char inputBuf1[8];
     memset(inputBuf1, 0, sizeof(inputBuf1));
 
-    const char ib1fmt[] = {'%', sizeof(inputBuf1) - 1 + '0', 's', 0};
+    //"%7s"
+    const char iB1Fmt[] = {'%', sizeof(inputBuf1) - 1 + '0', 's', 0};
+
+    int scanfResult = -1;
+    int fflushResult = -1;
 
     printf("Hello World!\r\n");
 
-    scanf(ib1fmt, inputBuf1);
-    printf("%s\r\n", inputBuf1);
+    printf("\r\nPress any key to continue.\r\n");
+    memset(inputBuf1, 0, sizeof(inputBuf1));
+    scanfResult = scanf(iB1Fmt, inputBuf1);
+    fflushResult = fflush(stdin);
+    printf("scanfResult=%i|fflushResult=%i|len=%li|%s|\r\n", scanfResult, fflushResult, strlen(inputBuf1), inputBuf1);
 
     printf("\r\nPress any key to exit.\r\n");
     memset(inputBuf1, 0, sizeof(inputBuf1));
-    scanf(ib1fmt, inputBuf1);
-    printf("%s\r\n", inputBuf1);
+    scanfResult = scanf(iB1Fmt, inputBuf1);
+    fflushResult = fflush(stdin);
+    printf("scanfResult=%i|fflushResult=%i|len=%li|%s|\r\n", scanfResult, fflushResult, strlen(inputBuf1), inputBuf1);
 
     usleep(3 * 1000 * 1000);
     return 0;
