338d2569531c463fe9b58b8da316232130053d2c
Yang <101186896+GNAySolution@users.noreply.github.com>
2022-12-26 23:26:25 +0800

    Update UnitTest001.cpp

--- Cpp/UnitTest001.cpp
+++ Cpp/UnitTest001.cpp
@@ -1,15 +1,7 @@
 #include <cstring>
-#include <stdio.h>
 #include <unistd.h>
 
-void CleanStdin()
-{
-    int c;
-
-    do {
-        c = getchar();
-    } while (c != '\n' && c != EOF);
-}
+#include "Tools/Functions.h"
 
 int main(const int argc, const char *argv[])
 {
@@ -23,6 +15,8 @@ int main(const int argc, const char *argv[])
     printf("sizeof(float)=%i\r\n", sizeof(float));
     printf("sizeof(double)=%i\r\n", sizeof(double));
     printf("sizeof(void *)=%i\r\n", sizeof(void *));
+    printf("sizeof(struct tm)=%i\r\n", sizeof(struct tm));
+    printf("sizeof(struct timeval)=%i\r\n", sizeof(struct timeval));
 
     char inputBuf1[8];
     memset(inputBuf1, 0, sizeof(inputBuf1));
@@ -35,13 +29,13 @@ int main(const int argc, const char *argv[])
     printf("\r\nPress any key to continue.\r\n");
     memset(inputBuf1, 0, sizeof(inputBuf1));
     scanfResult = scanf(iB1Fmt, inputBuf1);
-    CleanStdin();
+    Tools::CleanStdin();
     printf("scanfResult=%i|len=%li|input=%s|\r\n", scanfResult, strlen(inputBuf1), inputBuf1);
 
     printf("\r\nPress any key to exit.\r\n");
     memset(inputBuf1, 0, sizeof(inputBuf1));
     scanfResult = scanf(iB1Fmt, inputBuf1);
-    CleanStdin();
+    Tools::CleanStdin();
     printf("scanfResult=%i|len=%li|input=%s|\r\n", scanfResult, strlen(inputBuf1), inputBuf1);
 
     usleep(8 * 1000 * 1000);
