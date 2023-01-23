64fb6efa18ecbd7bf0b7e47f92d690e601348e73
GNAySolution <101186896+GNAySolution@users.noreply.github.com>
2023-01-23 08:33:37 +0800

    Update Functions.h

--- Cpp/Tools/Functions.h
+++ Cpp/Tools/Functions.h
@@ -1,10 +1,12 @@
 #ifndef _TOOLS_FUNCTIONS_H
 #define _TOOLS_FUNCTIONS_H
 
-#include <stdint.h>
+#include <iostream>
+#include <sstream>
 #include <stdio.h>
 #include <string.h>
 #include <sys/time.h>
+#include <thread>
 
 namespace Tools
 {
@@ -15,6 +17,34 @@ namespace Tools
         do {
             c = getchar();
         } while (c != '\n' && c != EOF);
+    }
+    
+    const uint64_t GetThreadID(const std::thread::id& threadID)
+    {
+        std::stringstream ss;
+
+        ss << threadID;
+        
+        return std::stoull(ss.str());
+    }
+
+    const uint64_t GetThreadID()
+    {
+        return GetThreadID(std::this_thread::get_id());
+    }
+
+    const int GetThreadIDAndStr(uint64_t& threadID, char *buffer, int& length)
+    {
+        if (threadID <= 0)
+        {
+            threadID = GetThreadID();
+        }
+        else
+        {
+            return length;
+        }
+
+        return length = sprintf(buffer, "%ld", threadID);
     }
 
     const struct tm *GetLocalTime(const time_t& rawTime)
