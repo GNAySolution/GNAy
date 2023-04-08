21a27789382c67c82e49f3275e410b617a970b43
GNAySolution <101186896+GNAySolution@users.noreply.github.com>
2023-04-08 18:01:29 +0800

    Update .NET Framework from 4.7 to 4.8
    Update .NET Framework from 4.7 to 4.8

--- GNAy.Tools.WPF/GNAy.Tools.NET48/Properties/AssemblyInfo.cs
+++ GNAy.Tools.WPF/GNAy.Tools.NET48/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("GNAy.Tools.NET47")]
+[assembly: AssemblyTitle("GNAy.Tools.NET48")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GNAy.Tools.NET47")]
+[assembly: AssemblyProduct("GNAy.Tools.NET48")]
 [assembly: AssemblyCopyright("Yang's Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 您可以指定所有的值，也可以使用 '*' 將組建和修訂編號
 // 設為預設，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.22.1203.11")]
-[assembly: AssemblyFileVersion("1.22.1203.11")]
+[assembly: AssemblyVersion("1.23.408.11")]
+[assembly: AssemblyFileVersion("1.23.408.11")]
