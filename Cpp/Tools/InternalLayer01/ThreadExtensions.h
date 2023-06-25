fd03a1695ae850a5e71a903dc1c540fcc780dcfd
GNAySolution <101186896+GNAySolution@users.noreply.github.com>
2023-06-25 09:56:33 +0800

    Update ThreadExtensions.h

--- Cpp/Tools/InternalLayer01/ThreadExtensions.h
+++ Cpp/Tools/InternalLayer01/ThreadExtensions.h
@@ -13,25 +13,23 @@ namespace Tools
     class IL01ThreadExtensions
     {
         protected:
-            static const long long GetIDValue()
+            static const std::string GetIDStr()
             {
                 const std::thread::id& _tID = std::this_thread::get_id();
 
                 std::stringstream ss;
                 ss << _tID;
 
-                const std::string str = ss.str();
-
-                printf("TRACE|Thread %s is initialized.\r\n", str.c_str());
-
-                return std::stoll(str);
+                return ss.str();
             }
 
         public:
+            static thread_local const std::string IDStr;
             static thread_local const long long IDValue;
     };
 
-    thread_local const long long IL01ThreadExtensions::IDValue = IL01ThreadExtensions::GetIDValue();
+    thread_local const std::string IL01ThreadExtensions::IDStr = IL01ThreadExtensions::GetIDStr();
+    thread_local const long long IL01ThreadExtensions::IDValue = std::stoll(IL01ThreadExtensions::IDStr);
 }
 
 #pragma pack()
