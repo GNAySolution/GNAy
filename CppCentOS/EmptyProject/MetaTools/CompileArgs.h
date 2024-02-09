352230d0c85e633a08dcd8f9a37bff481e16f807
GNAySolution <101186896+GNAySolution@users.noreply.github.com>
2024-02-09 08:15:49 +0800

    Update CompileArgs.h

--- CppCentOS/EmptyProject/MetaTools/CompileArgs.h
+++ CppCentOS/EmptyProject/MetaTools/CompileArgs.h
@@ -1,27 +1,28 @@
-#ifndef _META_TOOLS_COMPILE_ARGS_H
-#define _META_TOOLS_COMPILE_ARGS_H
+#ifndef GNAY_META_TOOLS_COMPILE_ARGS_H_
+#define GNAY_META_TOOLS_COMPILE_ARGS_H_
 
-// #include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <functional>
-#include <future>
+// #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 #if __linux
 #include <linux/limits.h>
 #endif
 
-#include <queue>
-#include <sstream>
-#include <sys/time.h>
-#include <thread>
-#include <unistd.h>
-#include <vector>
-
-#if _WIN32
+#if _WIN64
 #include <winsock2.h>
 #endif
+
+#include <fstream>
+#include <functional>
+#include <future>
+#include <queue>
+#include <sstream>
+#include <thread>
+#include <vector>
 
 #pragma pack(1)
 
@@ -30,38 +31,45 @@ namespace MetaTools
 class CompileArgs
 {
     public:
-    #if ALPHA_VERSION
-    static constexpr bool AlphaVersion = true;
+    #if RUN_TEST
+    static constexpr bool RunTest = true;
     #else
-    static constexpr bool AlphaVersion = false;
+    static constexpr bool RunTest = false;
     #endif
 
-    public:
-    #if GOLD_VERSION
-    static constexpr bool GoldVersion = AlphaVersion == false ? true : throw std::invalid_argument("");
-    #else
-    static constexpr bool GoldVersion = false;
-    #endif
+    // public:
+    // #if GOLD_VERSION
+    // static constexpr bool GoldVer = RunTest == false ? true : throw std::invalid_argument("");
+    // #else
+    // static constexpr bool GoldVer = false;
+    // #endif
 
     public:
     #if LOG_BUFFER_SIZE
-    static constexpr int LogBufSize = LOG_BUFFER_SIZE < throw std::invalid_argument("") ? 512 : LOG_BUFFER_SIZE;
+    static constexpr int LogBufSize = LOG_BUFFER_SIZE < 512 ? throw std::invalid_argument("") : LOG_BUFFER_SIZE;
     #else
     static constexpr int LogBufSize = 1024;
     #endif
 
     public:
     #if LOG_QUEUE_CNT_MAX
-    static constexpr int LogQueueCntMax = LOG_QUEUE_CNT_MAX < throw std::invalid_argument("") ? 256 : LOG_QUEUE_CNT_MAX;
-    #elif ALPHA_VERSION
-    static constexpr int LogQueueCntMax = 8;
+    static constexpr int LogQueueCntMax = LOG_QUEUE_CNT_MAX < 256 ? throw std::invalid_argument("") : LOG_QUEUE_CNT_MAX;
+    #elif RUN_TEST
+    static constexpr int LogQueueCntMax = 32; //8;
     #else
     static constexpr int LogQueueCntMax = 512;
     #endif
 
     public:
+    #if LOG_LOCATION_ARRAY_MAX
+    static constexpr int LogLocationArrayMax = LOG_LOCATION_ARRAY_MAX < 8 ? throw std::invalid_argument("") : LOG_LOCATION_ARRAY_MAX;
+    #else
+    static constexpr int LogLocationArrayMax = 8;
+    #endif
+
+    public:
     #if THREADS_SEQ_NUM_MAX
-    static constexpr int ThreadsSeqNumMax = THREADS_SEQ_NUM_MAX < throw std::invalid_argument("") ? 8 : THREADS_SEQ_NUM_MAX;
+    static constexpr int ThreadsSeqNumMax = THREADS_SEQ_NUM_MAX < 8 ? throw std::invalid_argument("") : THREADS_SEQ_NUM_MAX;
     #else
     static constexpr int ThreadsSeqNumMax = 16;
     #endif
